af500edf429dcf609085e2035c96f1d6d328a837
Hyewonleeeee <llys7095@naver.com>
2025-11-02 21:57:57 +0900

    Update Skills.tsx

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const skillCategories = [
   {
     title: "Audio Tools",
-    skills: ["DAW", "JUCE", "Max"],
+    skills: ["Ableton", "Logic", "Cubase", "JUCE", "Max"],
   },
   {
     title: "Development",
